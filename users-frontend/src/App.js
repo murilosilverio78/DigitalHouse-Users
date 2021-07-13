4d401a1a917238695b0b8f4905ea449c4d886021
murilosilverio78 <murilo.silverio@hotmail.com>
2021-07-12 21:45:07 -0300

    commit final

--- users-frontend/src/App.js
+++ users-frontend/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { useState } from 'react';
 
 import { getCepData } from './services/viacep';
+import Pagination from './pagination';
 
 function App() {
   const [cep, setCep] = useState('');
   const [logradouro, setLogradouro] = useState('');
+  const [page, setPage] = useState(2)
   
   async function onBlurCep() {          
     const data = await getCepData(cep);
@@ -42,7 +44,9 @@ function App() {
         <input id="numero" type="text" placeholder="Digite o número da casa"/> 
        
         <button id="signupBtn" type="submit">Cadastrar</button>
+        <Pagination page = {page} onPageChange = {(newPage) => setPage(newPage)} />
       </form>   
+      
     </div>         
   );
 }
